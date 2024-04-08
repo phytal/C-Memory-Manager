aa3e4ab51c4f4bb1362b3d332b663e2bc730b203
William Zhang <willzhang@utexas.edu>
2024-04-07 20:59:17 -0500

    test gc

--- libtdmm/tdmm.c
+++ libtdmm/tdmm.c
@@ -374,7 +374,7 @@ void t_gcollect (void) {
     char *temp;
     // char *sp = temp + (size_t)(char*)stack_bottom;
 
-    void* sp = &temp + (size_t)(char*)stack_bottom;
+    void* sp = temp + (size_t)(char*)stack_bottom;
     for (char* start = sp; start < (char*)stack_bottom; start++) { 
         check_valid_pointer((long*)((void*)start)); // Check if the pointer is valid
     }
